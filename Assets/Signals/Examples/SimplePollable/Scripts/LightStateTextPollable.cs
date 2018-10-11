9db8e28ce09808d39f2fd8066aa6f3fea201e86c
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-10-11 18:13:00 +0100

    Moved some files around

---
+++ Assets/Signals/Examples/SimplePollable/Scripts/LightStateTextPollable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightStateTextPollable : MonoBehaviour {
+
+	[SerializeField]
+	BoolPollable lightState;
+
+	[SerializeField]
+	TextMesh textField;
+	
+	void Start()
+	{
+		UpdateText();
+	}
+
+	void Update () 
+	{
+		if( lightState.HasChanged() ){
+			UpdateText();
+		}	
+	}
+
+	void UpdateText()
+	{
+		textField.text = lightState.Value ? "On" : "Off" ;
+	}
+}
